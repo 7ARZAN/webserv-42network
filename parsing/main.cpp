524198545ea8455c461c89cf09ddef1fa323b9c9
elhousseine lakhfif <elakhfif@c1r6p5.1337.ma>
2024-04-06 02:06:48 +0000

    webserv

--- parsing/main.cpp
+++ parsing/main.cpp
@@ -6,16 +6,21 @@
 /*   By: tarzan <elakhfif@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 20:51:06 by tarzan            #+#    #+#             */
-/*   Updated: 2024/03/20 02:44:16 by elakhfif         ###   ########.fr       */
+/*   Updated: 2024/03/30 02:20:32 by elakhfif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "config_file.hpp"
+#include <fcntl.h>
 
 int	main()
 {
 	ConfigFile config("config.conf");
 
+	if (open("config.conf", O_RDONLY) == -1){
+		std::cout << "File config.conf not found" << std::endl;
+		return 1;
+	}
 	//redirected to the file meme.txt
 	config.redirect("meme.txt");
 	config.checker("server", "http://localhost:8080");
