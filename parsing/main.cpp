b8983bcb9295738c7b3035492858a17471ec93d0
elhousseine lakhfif <elakhfif@c1r4p1.1337.ma>
2024-04-30 00:13:45 +0100

    humm

--- parsing/main.cpp
+++ parsing/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tarzan <elakhfif@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 20:51:06 by tarzan            #+#    #+#             */
-/*   Updated: 2024/03/30 02:20:32 by elakhfif         ###   ########.fr       */
+/*   Updated: 2024/04/07 03:03:47 by elakhfif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@ int	main()
 	}
 	//redirected to the file meme.txt
 	config.redirect("meme.txt");
-	config.checker("server", "http://localhost:8080");
-	config.checker("port", "8080");
+	//config.checker("server", "http://localhost:8080");
+	//config.checker("port", "8080");
 	config.save();
 	//config.print();
 	return 0;
